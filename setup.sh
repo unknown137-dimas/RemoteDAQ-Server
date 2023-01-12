704a43b83e1998f6eef6216afdb6debba6bd93b4
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-01-12 15:35:59 +0700

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Add Docker Repository
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Install Docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#Install Zerotier Client
+curl -s https://install.zerotier.com | sudo bash
+zerotier-cli join $1
+
+#Deploy
+docker-compose up -d
