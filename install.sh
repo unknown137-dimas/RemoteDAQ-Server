160e76c859669caf99044f9c130f8a8e2069e2fa
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-02-12 13:40:07 +0700

    Update install script & server setup playbook

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install Ansible
-sudo apt update
-sudo apt install -y software-properties-common
+sudo apt -qq update
+sudo apt install -yq software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+sudo apt install -yq ansible
 
 # Install RemoteDAQ Server
 sudo ansible-playbook remotedaq_server_setup.yml
