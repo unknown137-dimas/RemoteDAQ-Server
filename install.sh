94bfba7a7afc21c203e8467d5656c7012774f8b5
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-02-12 13:58:26 +0700

    Make install script output more quiet

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Install Ansible
+echo "Installing Ansible..."
 sudo apt -qq update
 sudo apt install -yq software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible > /dev/null 2>&1
 sudo apt install -yq ansible
+sudo apt autoremove
 
 # Install RemoteDAQ Server
+echo "Configuring RemoteDAQ Server..."
 sudo ansible-playbook remotedaq_server_setup.yml
