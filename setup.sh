35ff14b50022a167a96b5b3eb8392b59f6f66984
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-05-26 09:47:37 +0700

    Fix dependencies error in setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "Installing Ansible..."
 sudo apt update > /dev/null 2>&1
 sudo apt install -y software-properties-common > /dev/null 2>&1
 sudo apt-add-repository -y ppa:ansible/ansible > /dev/null 2>&1
-sudo apt install -y ansible > /dev/null 2>&1
+sudo apt install -y ansible-core ansible > /dev/null 2>&1
 sudo apt -y autoremove > /dev/null 2>&1
 
 # Setup RemoteDAQ Server
